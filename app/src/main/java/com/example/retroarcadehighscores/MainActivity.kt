30bd73544ef4181121508fc670d17a3e46b9405f
Corey Kuether <coreykuether1@gmail.com>
2022-01-03 00:12:23 -0500

    updated app

--- app/src/main/java/com/example/retroarcadehighscores/MainActivity.kt
+++ app/src/main/java/com/example/retroarcadehighscores/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.retroarcadehighscores
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.retroarcadehighscores.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toolbar.toolbar.title = "Retro Arcade Scoreboard"
+        binding.toolbar.toolbar.navigationIcon = ContextCompat.getDrawable(this, R.drawable.ic_arrow_back)
+        binding.toolbar.toolbar.setNavigationOnClickListener {
+            super.onBackPressed()
+        }
+
+        if(resources.getBoolean(R.bool.isTablet)){
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
     }
 }
